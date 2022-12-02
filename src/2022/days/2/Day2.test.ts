6be8bf19e9995988890f11b146d0a78860a338dc
Tukajo <tjfrisch@gmail.com>
2022-12-02 16:28:07 -0600

    - Day 2 part 1 initial commit.

--- src/2022/days/2/Day2.test.ts
+++ src/2022/days/2/Day2.test.ts
@@ -4,8 +4,12 @@ const fullFilePath = __dirname + '/test_input.txt';
 const testInput = readFile(fullFilePath);
 
 // Test the day2 function
-import { solutionPartOne } from './Day2';
+import { solutionPartOne, solutionPartTwo } from './Day2';
 
 test('Day 2 Part 1', () => {
     expect(solutionPartOne(testInput)).toBe(14827);
+});
+
+test('Day 2 Part 2', () => {
+    expect(solutionPartTwo(testInput)).toBe(207968);
 });
--- src/2022/days/2/Day2.ts
+++ src/2022/days/2/Day2.ts
@@ -7,6 +7,22 @@ const ShapeMap: Record<string, string> = {
     Z: 'Scissors',
 };
 
+const KeyWinsAgainstValue: Record<string, string> = {
+    Rock: 'Paper',
+    Paper: 'Scissors',
+    Scissors: 'Rock',
+};
+const ValueWinsAgainstKey: Record<string, string> = {
+    Rock: 'Scissors',
+    Paper: 'Rock',
+    Scissors: 'Paper',
+};
+const PlayResponseMap: Record<string, (arg: string) => string> = {
+    X: (playerOneChoice: string) => ValueWinsAgainstKey[playerOneChoice],
+    Y: (playerOneChoice: string) => playerOneChoice,
+    Z: (playerOneChoice: string) => KeyWinsAgainstValue[playerOneChoice],
+};
+
 const ScoreMap: Record<string, number> = {
     Rock: 1,
     Paper: 2,
@@ -33,6 +49,22 @@ const decideWinner = (playerOneChoice: string, playerTwoChoice: string): number 
     return 1;
 };
 
+const evaluatePlay = (playerOneShape: string, playerTwoShape: string): number[] => {
+    const roundScores = [0, 0];
+    roundScores[0] += ScoreMap[playerOneShape];
+    roundScores[1] += ScoreMap[playerTwoShape];
+    const winnerIndex = decideWinner(playerOneShape, playerTwoShape);
+    if (winnerIndex === -1) {
+        // If there was a tie, then both players get 3 points
+        roundScores[0] += 3;
+        roundScores[1] += 3;
+    } else {
+        // Otherwise, increase the winner's score by 6
+        roundScores[winnerIndex] += ScoreMap.Victory;
+    }
+    return roundScores;
+};
+
 export const solutionPartOne = (input: string): number => {
     const rounds: string[] = input.split('\r\n');
     // initialize the scores
@@ -40,18 +72,25 @@ export const solutionPartOne = (input: string): number => {
     rounds.forEach((round) => {
         const playerOneShape = ShapeMap[round[0]];
         const playerTwoShape = ShapeMap[round[2]];
-        // Increase score based on the shape, each player always scores their shape.
-        scores[0] += ScoreMap[playerOneShape];
-        scores[1] += ScoreMap[playerTwoShape];
-        const winnerIndex = decideWinner(playerOneShape, playerTwoShape);
-        if (winnerIndex === -1) {
-            // If there was a tie, then both players get 3 points
-            scores[0] += 3;
-            scores[1] += 3;
-        } else {
-            // Otherwise, increase the winner's score by 6
-            scores[winnerIndex] += ScoreMap.Victory;
-        }
+        const roundScores = evaluatePlay(playerOneShape, playerTwoShape);
+        scores[0] += roundScores[0];
+        scores[1] += roundScores[1];
+    });
+
+    return scores[1];
+};
+
+export const solutionPartTwo = (input: string): number => {
+    // initialize the scores
+    const rounds: string[] = input.split('\r\n');
+    // initialize the scores
+    const scores = [0, 0];
+    rounds.forEach((round) => {
+        const playerOneShape = ShapeMap[round[0]];
+        const playerTwoShape = PlayResponseMap[round[2]](playerOneShape);
+        const roundScores = evaluatePlay(playerOneShape, playerTwoShape);
+        scores[0] += roundScores[0];
+        scores[1] += roundScores[1];
     });
 
     return scores[1];
