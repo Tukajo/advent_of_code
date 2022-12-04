970d056d7b77b48c528d4163d5a09e611cc47532
Tukajo <tjfrisch@gmail.com>
2022-12-04 10:49:29 -0600

    -- Day 2 part 2 test

---
+++ src/2022/days/4/Day4.test.ts
@@ -0,0 +1,15 @@
+// read the input from test_input.txt
+import { readFile } from '../../common/FileReader';
+const fullFilePath = __dirname + '/test_input.txt';
+const testInput = readFile(fullFilePath);
+
+// Test the day1 function
+import { solutionPartOne, solutionPartTwo } from './Day4';
+
+test('Day 4 Part 1', () => {
+    expect(solutionPartOne(testInput)).toBe(576);
+});
+
+test('Day 4 Part 2', () => {
+    expect(solutionPartTwo(testInput)).toBe(207968);
+});
