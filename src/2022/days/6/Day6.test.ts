c4cb2c70a8077249cfd2721ba69f34a9f0ec2dcf
Tukajo <tjfrisch@gmail.com>
2022-12-06 13:22:01 -0600

    -- Day 6 part 1 & 2... could do better.

---
+++ src/2022/days/6/Day6.test.ts
@@ -0,0 +1,15 @@
+// read the input from test_input.txt
+import { readFile } from '../../common/FileReader';
+const fullFilePath = __dirname + '/test_input.txt';
+const testInput = readFile(fullFilePath);
+
+// Test the day1 function
+import { solutionPartOne, solutionPartTwo } from './Day6';
+
+test('Day 6 Part 1', () => {
+    expect(solutionPartOne(testInput)).toBe(1566);
+});
+
+test('Day 6 Part 2', () => {
+    expect(solutionPartTwo(testInput)).toBe(2265);
+});
