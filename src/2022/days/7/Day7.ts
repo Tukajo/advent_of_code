02f2c8ae36e8497dabd8d34fd3a52d196ad6e6aa
Tukajo <tjfrisch@gmail.com>
2022-12-07 12:42:34 -0600

    -- Day 7 init

---
+++ src/2022/days/7/Day7.ts
@@ -0,0 +1,12 @@
+
+export const solutionPartOne = (input: string): number => {
+    //TODO
+    console.log(input);
+    return 0;
+};
+
+export const solutionPartTwo = (input: string): number => {
+    //TODO
+    console.log(input);
+    return 0;
+};
